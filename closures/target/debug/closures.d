14bb7b523ced50a81870a90617c7ef208a01aba3
isaacddr <isaacovic00@gmail.com>
2019-04-23 13:49:19 -0500

    closures

---
+++ closures/target/debug/closures.d
@@ -0,0 +1,1 @@
+/home/neo/Documents/rusteando/closures/target/debug/closures: /home/neo/Documents/rusteando/closures/src/main.rs
---
+++ closures/target/debug/deps/closures-e5c58140dc427996.d
@@ -0,0 +1,5 @@
+/home/neo/Documents/rusteando/closures/target/debug/deps/closures-e5c58140dc427996: src/main.rs
+
+/home/neo/Documents/rusteando/closures/target/debug/deps/closures-e5c58140dc427996.d: src/main.rs
+
+src/main.rs:
