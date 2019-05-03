a760e6981ccc9408e5620ae2100f9b55f6a8ff89
isaacddr <isaacovic00@gmail.com>
2019-05-02 19:01:38 -0500

    nothin

---
+++ testing1/target/debug/deps/testing1-12d58b4a3c3ca870.d
@@ -0,0 +1,5 @@
+/home/neo/Documents/rusteando/testing1/target/debug/deps/testing1-12d58b4a3c3ca870.rmeta: src/main.rs
+
+/home/neo/Documents/rusteando/testing1/target/debug/deps/testing1-12d58b4a3c3ca870.d: src/main.rs
+
+src/main.rs:
---
+++ testing1/target/debug/deps/testing1-3e2af0c6d25b8871.d
@@ -0,0 +1,5 @@
+/home/neo/Documents/rusteando/testing1/target/debug/deps/testing1-3e2af0c6d25b8871: src/main.rs
+
+/home/neo/Documents/rusteando/testing1/target/debug/deps/testing1-3e2af0c6d25b8871.d: src/main.rs
+
+src/main.rs:
